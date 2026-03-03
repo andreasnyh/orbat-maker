21d7ec7aec5f47edf550471a19bbd119f6e5b3a7
Andreas Ny <andreas.nyh87@gmail.com>
2026-03-03 18:44:54 +0100

    Set base path dynamically based on NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/orbat-maker/',
+  base: process.env.NODE_ENV === 'production' ? '/orbat-maker/' : '/',
   plugins: [
     react(),
     tailwindcss(),
